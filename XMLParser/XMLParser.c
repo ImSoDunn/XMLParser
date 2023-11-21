39a4beae8e3c8dacebe02337e7c47325dfd305d5
Bryan Dunn <76450960+ImSoDunn@users.noreply.github.com>
2023-11-21 12:16:59 -0600

    Initial Release of XMLParser
    Initial Code dump

---
+++ XMLParser/XMLParser.c
@@ -0,0 +1,52 @@
+#include <stdio.h>
+#include <libxml/parser.h>
+#include <libxml/tree.h>
+
+
+void parseXML(xmlNodePtr node){
+        xmlNodePtr current = node;
+
+        while (current != NULL){
+                if (current->type == XML_ELEMENT_NODE){
+                        if (xmlStrcmp(current->name, (const xmlChar *)"title") == 0){
+                                printf("Title: %s\n", xmlNodeGetContent(current));
+                        }
+                }
+
+
+        //Recursive to parse child nodes
+        parseXML(current->children);
+
+        //Set to next for loop
+        current = current->next;
+
+        }
+}
+
+
+
+
+int main() {
+        xmlDocPtr doc;
+        xmlNodePtr root;
+
+        // Parse the XML file
+        doc = xmlReadFile("catalog.xml", NULL, 0);
+
+        if (doc == NULL) {
+                fprintf(stderr, "Failed to parse the document.\n");
+                return 1;
+        }
+
+        // Get the root element, this will be a xmlNodePtr
+        root = xmlDocGetRootElement(doc);
+
+        //Parse the XML for the Title
+        parseXML(root);
+
+        // Free the Resources
+        xmlFreeDoc(doc);
+        xmlCleanupParser();    
+        
+        return 0;
+}
